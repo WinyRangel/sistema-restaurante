bb80ed4fc8eacc1ac8eba2bf77bc6e5039113c66
HernandezChiquitoSandra <107783594+HernandezChiquitoSandra@users.noreply.github.com>
2024-12-07 12:54:16 -0600

    se quito lo de aws

--- server/config/db.js
+++ server/config/db.js
@@ -5,15 +5,15 @@ require('dotenv').config();
 const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
 const PAYPAL_API = process.env.PAYPAL_API; // URL sandbox or live for your app
-const HOST = 'http://3.128.24.47:3002/api/payment';
+const HOST = 'http://localhost:3002/api/payment';
 
 const connectDB = async () => {
 
   try {
     const connection = await mysql.createConnection({
-      host: 'sistema-restaurante.cp6o84ukgbxf.us-east-2.rds.amazonaws.com',
-      user: 'admin',             
-      password: 'sistema2*',  
+      host: 'localhost',
+      user: 'root',             
+      password: '',  
       database: 'sistemaPedidos',   
       port: 3306             
     });
